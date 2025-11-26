93374a9321b2143fde4bd676d2e6a1a651962679
Manish Dhull <manishdhull1990@gmail.com>
2025-11-26 23:33:37 +0530

    Client sever

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import ToolMessage
+import json
 
 load_dotenv()
 
@@ -12,6 +13,10 @@ SERVERS = {
         "transport": "stdio",
         "command": "C:\\Python313\\Scripts\\uv.exe",
         "args": ["run", "fastmcp", "run", "D:/mcp_math_server/main.py"]
+    },
+    "expense": {
+        "transport": "streamable_http",
+        "url": "https://soft-black-jellyfish.fastmcp.app/mcp"
     }
 }
 async def main():
@@ -24,31 +29,37 @@ async def main():
         named_tools[tool.name] = tool
 
     #print(named_tools)
+    print('Available tools:', named_tools.keys())
 
     llm = ChatOpenAI(model="gpt-5")
     llm_with_tools = llm.bind_tools(tools)
 
-    # prompt = "What is the product of 12 and 15 using the math tool?"
-    prompt = "What is the capital of India?"
+    prompt = "Add an expense - Rs 800 for groceries on 26th Nov"
+    #prompt = "What is the capital of India?"
     response = await llm_with_tools.ainvoke(prompt)
 
     if not getattr(response, "tool_calls", None):
         print("\nLLM Reply: ",response.content)
         return 
     
-    selected_tool = response.tool_calls[0]["name"]
-    selected_tool_args = response.tool_calls[0]["args"]
-    selected_tool_id = response.tool_calls[0]["id"]
+    tool_message = []
+    for tc in response.tool_calls:
+        selected_tool = tc["name"]
+        selected_tool_args = tc.get("args") or {}
+        selected_tool_id = tc["id"]
 
-    # print(f"Selected tool: {selected_tool}")
-    # print(f"Selected tool args: {selected_tool_args}")
+        print(f"\n-> Executing remote tool: {selected_tool}")
+        print(f" with args: ", selected_tool_args)
 
-    tool_result = await named_tools[selected_tool].ainvoke(selected_tool_args)
-    # print(f"Tool result: {tool_result}")
+        # print(f"Selected tool: {selected_tool}")
+        # print(f"Selected tool args: {selected_tool_args}")
 
-    tool_message = ToolMessage(content=tool_result, tool_call_id=selected_tool_id)
-    final_response = await llm_with_tools.ainvoke([prompt, response, tool_message])
+        result = await named_tools[selected_tool].ainvoke(selected_tool_args)
+        # print(f"Tool result: {tool_result}")
 
+        tool_message.append(ToolMessage(tool_call_id=selected_tool_id, content = json.dumps(result)))
+    
+    final_response = await llm_with_tools.ainvoke([prompt, response, *tool_message])
     print(f"Final response: {final_response.content}")
     
 if __name__ == "__main__":
